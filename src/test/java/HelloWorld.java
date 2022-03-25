b1af27cbe98637e6bac2d132c0dfa003635eb510
brustyn <bartosz.rustyn@pl.abb.com>
2022-03-25 19:42:59 +0100

    First commit

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String [] args) {
+
+        System.out.println("Hello world");
+        wypisz();
+    }
+
+    public static void wypisz(){
+        System.out.println("Putin chuj");
+    }
+
+}
